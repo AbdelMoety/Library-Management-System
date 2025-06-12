c0efb4b5d32183e878eab0596037771a6f66099c
sohaila-emad <sohaila.abdelmageed05@eng-st.cu.edu.eg>
2025-06-12 17:07:29 +0300

    Added history tracking for borrowed books + due date logic

--- src/models/Student.java
+++ src/models/Student.java
@@ -1,25 +1,27 @@
 // class for students containing every student's info
 package models;
 
-import Data_Structures.LL;
+import java.util.ArrayList;
 
-public class Student
-{
+import Data_Structures.LL;
+import models.borrowedBook;
+
+public class Student {
     public String name;
     public int id;
     public int year;
     public LL borrowHistory;
     public int borrowcount;
-    public static int maxBorrow= 3;
+    public static int maxBorrow = 3;
 
-    public Student(String name, int id, int year)
-    {
+    public ArrayList<borrowedBook> history = new ArrayList<>();
+
+    public Student(String name, int id, int year) {
         this.name = name;
         this.id = id;
         this.year = year;
         this.borrowHistory = new LL();
         this.borrowcount = 0;
     }
-
 
 }
